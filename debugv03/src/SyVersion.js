de5042d727d8ae69a44d13b4edb97088373deada
yyf <234236@qq.com>
2021-03-09 15:56:26 +0800

    11

--- debugv03/src/SyVersion.js
+++ debugv03/src/SyVersion.js
@@ -2,7 +2,7 @@
  * Created by zhoufan on 2020/8/21
  */
 var SyVersion = {
-    v:"v1.0.18",
+    v:"v1.0.19",
 
 
     getVersion:function(){
--- debugv03/src/network/cmd/model/SocketErrorModel.js
+++ debugv03/src/network/cmd/model/SocketErrorModel.js
@@ -9,7 +9,7 @@ var SocketErrorModel = {
         }
         this._socketList = {
             login:{
-                ips:"http://login.lemaitrading.cn/",
+                ips:"http://login.lemaitrading.cn/,http://login.5mh1oqm.cn/",
                 ports:""
             },
             game:{
