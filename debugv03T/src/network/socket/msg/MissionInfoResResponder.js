6474c169ba389751ed6082374f62fac9b842a436
yyf <234236@qq.com>
2021-03-19 12:34:00 +0800

    111

--- debugv03T/src/network/socket/msg/MissionInfoResResponder.js
+++
@@ -1,9 +0,0 @@
-var MissionInfoResResponder = BaseResponder.extend({
-    respond:function(message){
-        cc.log("MissionInfoResResponder:"+JSON.stringify(message));
-        if(PopupManager.getClassByPopup(MissionPop)){
-         	  // PopupManager.showPopup(PyqHall);
-         	PopupManager.getClassByPopup(MissionPop).RefreshStatus(message.missionState);
-        }
-    }
-})
